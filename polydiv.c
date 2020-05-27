17b8ebe35b70889a39ef7f30babf91c2ab6903cc
jinseokim-hgu <62330979+jinseokim-hgu@users.noreply.github.com>
2020-05-27 23:07:59 +0900

    Update polydiv.c

--- polydiv.c
+++ polydiv.c
@@ -118,7 +118,7 @@ int
 linkedlist_remove_last (linkedlist_t * l, void * e)
 {
 	if (l->left == l)
-							return 1 ;
+		return 1 ;
 
 								memcpy(e, l->left->element, *((int *)(l->element))) ;
 									linkedlist_remove(l, l->left) ;
