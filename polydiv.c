d5354354654ac840f606fbe34f82de5307d80337
jinseokim-hgu <62330979+jinseokim-hgu@users.noreply.github.com>
2020-06-07 01:41:09 +0900

    Update polydiv.c

--- polydiv.c
+++ polydiv.c
@@ -245,7 +245,7 @@ polynomial_add (polynomial * p1, polynomial * p2)
 {
 	polynomial * r = linkedlist_alloc(sizeof(term_t)) ;
 
-				term_t t1, t2 ;
+	term_t t1, t2 ;
 					int i1 = 0 , i2 = 0 ;
 
 						while (i1 < linkedlist_length(p1) && i2 < linkedlist_length(p2)) {
