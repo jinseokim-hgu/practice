94be2f0d487f8b58cf649f18076849493c8cb7d6
jinseokim-hgu <62330979+jinseokim-hgu@users.noreply.github.com>
2020-06-07 01:46:20 +0900

    Update polydiv.c

--- polydiv.c
+++ polydiv.c
@@ -279,7 +279,7 @@ polynomial_add (polynomial * p1, polynomial * p2)
 		polynomial_add_term(r, &t2) ;
 		i2 += 1 ;
 	}
-																																																																																		return r ;
+	return r ;
 }
 
 polynomial *
